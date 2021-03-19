924ebfd6cd4cf5b1ae6b0dc769afa819b0e3246a
Ade Adeola <adedotster@gmail.com>
2021-03-19 14:17:19 +0000

    list of notes created

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,13 +1,62 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Layout from "../components/Layout";
+import firebase from "../firebase/init";
+export interface dirProps {
+  fullPath: string;
+  name: string;
+}
 
-export default function Help() {
+const StyledNotes = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+  margin: 6px;
+  &:hover {
+    color: #36689a;
+    text-decoration: underline;
+  }
+`;
+export default function Notes() {
+  const [dir, setDir] = useState<dirProps[]>([]);
+
+  useEffect(() => {
+    const list: dirProps[] = [];
+    firebase
+      .storage()
+      .ref("public")
+      .list({ maxResults: 100 })
+      .then((res) => {
+        res.prefixes.forEach((folderRef) => {
+          const { fullPath, name } = folderRef;
+          list.push({
+            fullPath,
+            name,
+          });
+        });
+      })
+      .then(() => setDir(list))
+      .catch((notesError) => {
+        console.log({ notesError });
+      });
+  }, []);
   return (
     <Layout active="notes">
-      <div>
-        This is where the notes are take will be placed. I will add subroutes,
-        by using .md or .mdx, and return dir of the target folder us "fs" and
-        lazy loading
-      </div>
+      <Header>Notes</Header>
+      <StyledNotes>
+        {dir.map(({ name }) => (
+          <StyledLink to={`notes/${name}`} key={name}>
+            {name}
+          </StyledLink>
+        ))}
+      </StyledNotes>
     </Layout>
   );
 }
